b47b9dc6b8019ba324a15c1d08a949ae20d3373a
Wu Justin <justin.cathay@gmail.com>
2021-06-10 20:14:43 +0800

    Adjust <SocilIcon>

--- src/components/SocialIcon/index.js
+++ src/components/SocialIcon/index.js
@@ -12,16 +12,16 @@ class SocialIcons extends React.Component {
   render() {
     return (
       <SocialWrap>
-        <SocialLink href="#">
+        <SocialLink href="https://www.facebook.com/JustinWu3737/" target="_blank">
           <FontAwesomeIcon icon={faFacebook}/>
         </SocialLink>
-        <SocialLink href="#">
+        <SocialLink href="https://github.com/justin3737" target="_blank">
           <FontAwesomeIcon icon={faGithub}/>
         </SocialLink>
-        <SocialLink href="#">
+        <SocialLink href="https://www.instagram.com/justin3737/" target="_blank">
           <FontAwesomeIcon icon={faInstagram}/>
         </SocialLink>
-        <SocialLink href="#">
+        <SocialLink href="https://www.flickr.com/photos/justin-image/albums" target="_blank">
           <FontAwesomeIcon icon={faFlickr}/>
         </SocialLink>
       </SocialWrap>
