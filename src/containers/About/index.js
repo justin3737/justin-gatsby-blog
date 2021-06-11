e2cd7c4a4cdb33de309407418e237ed16189df98
Wu Justin <justin.cathay@gmail.com>
2021-06-11 16:20:19 +0800

    adjust About page

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -2,17 +2,24 @@ import React from "react"
 import { AboutWrap } from "./about-comp"
 import SocialIcon from "../../components/SocialIcon"
 import Avator from "../../components/Avator"
-import { AuthorName, AuthorDesc, AuthorIntro, AboutLinearMask } from "./about-comp"
+import {
+  AuthorName,
+  AuthorDesc,
+  AuthorIntro,
+  AboutLinearMask,
+} from "./about-comp"
 
 class About extends React.Component {
   render() {
     return (
       <AboutWrap>
-        <AboutLinearMask/>
-        <Avator/>
+        <AboutLinearMask />
+        <Avator />
         <AuthorName>Justin</AuthorName>
         <AuthorDesc>前端工程師、業餘攝影愛好者</AuthorDesc>
-        <AuthorIntro>工業設計系畢業，喜歡登山/旅遊/攝影，凡是遇上有趣的研究便無法自拔廢寢忘食，目前正在前往全端工程師學習的路上，建立此blog，希望紀錄所見所學，更希望能在專案中發揮所學將設計與程式碼完美結合。</AuthorIntro>
+        <AuthorIntro>
+          工業設計系畢業，喜歡登山/旅遊/攝影，凡是遇上有趣的研究便無法自拔廢寢忘食，目前正在前往全端工程師學習的路上，建立此blog，希望紀錄所見所學，更希望能在專案中發揮所學將設計與程式碼完美結合。
+        </AuthorIntro>
         <SocialIcon />
       </AboutWrap>
     )
