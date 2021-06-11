6ff4ddf971ec900755ae81ffbec647775018b025
Wu Justin <justin.cathay@gmail.com>
2021-06-11 14:03:42 +0800

    add name

--- src/containers/About/about-comp.js
+++ src/containers/About/about-comp.js
@@ -6,3 +6,26 @@ export const AboutWrap = styled.div`
   background: url("/public/about_bg.jpg") center center no-repeat;
   background-size: cover;
 `
+
+export const AuthorName = styled.h1`
+  position: relative;
+  display: inline-block;
+  width: calc(100% - 92px);
+  margin: 1% 48px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 52px;
+  text-align: center;
+  color: #ffffff;
+  text-shadow: 0px 2px 8px rgba(0, 0, 0, 0.25);
+`
+
+export const AuthorDesc = styled.div`
+  position: relative;
+  margin: 0 48px;
+  text-align-last:justify;
+  color: #FFFFFF;
+  font-weight: 500;
+  text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.25);
+`
--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -2,12 +2,15 @@ import React from "react"
 import { AboutWrap } from "./about-comp"
 import SocialIcon from "../../components/SocialIcon"
 import Avator from "../../components/Avator"
+import { AuthorName, AuthorDesc } from "./about-comp"
 
 class About extends React.Component {
   render() {
     return (
       <AboutWrap>
         <Avator />
+        <AuthorName>Justin</AuthorName>
+        <AuthorDesc>前端工程師、業餘攝影愛好者</AuthorDesc>
         <SocialIcon />
       </AboutWrap>
     )
